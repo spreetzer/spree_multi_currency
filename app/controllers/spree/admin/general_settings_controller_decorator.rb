05b66edecde0a4c35f7dc9d79a6fe496d317aeff
spreetzer <56721336+spreetzer@users.noreply.github.com>
2019-10-18 12:28:20 +0200

    Update general_settings_controller_decorator.rb

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++ app/controllers/spree/admin/general_settings_controller_decorator.rb
@@ -1,24 +1,22 @@
-module Spree
-  module Admin
-    GeneralSettingsController.class_eval do
-      before_action :update_currency_settings, only: :update
+class Spree::Admin::GeneralSettingsControllerDecorator
+  before_action :update_currency_settings, only: :update
 
-      def render(*args)
-        @preferences_currency |= [:allow_currency_change, :show_currency_selector, :supported_currencies]
-        super
+  def render(*args)
+    @preferences_currency |= [:allow_currency_change, :show_currency_selector, :supported_currencies]
+    super
+  end
+
+  private
+
+  def update_currency_settings
+    params.each do |name, value|
+      next unless Spree::Config.has_preference? name
+      if name == 'supported_currencies'
+        value = value.split(',').map { |curr| ::Money::Currency.find(curr.strip).try(:iso_code) }.concat([Spree::Config[:currency]]).uniq.compact.join(',')
       end
-
-      private
-
-      def update_currency_settings
-        params.each do |name, value|
-          next unless Spree::Config.has_preference? name
-          if name == 'supported_currencies'
-            value = value.split(',').map { |curr| ::Money::Currency.find(curr.strip).try(:iso_code) }.concat([Spree::Config[:currency]]).uniq.compact.join(',')
-          end
-          Spree::Config[name] = value
-        end
-      end
+      Spree::Config[name] = value
     end
   end
 end
+
+(Spree::Admin::GeneralSettingsController.prepend Spree::Admin::GeneralSettingsControllerDecorator) if Spree.version.to_f >= 3.7
